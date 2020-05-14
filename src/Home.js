a8cca4fca5ae38f8a9a9e19786423bc05ad69a4a
wakkihaya <wakkihaya@gmail.com>
2020-05-14 17:29:41 +0900

    Next user after push buttons

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Home.scss';
 
 
-const People = () =>{
+const People = (userIndex) =>{
     const users = require('./user_sample.json');
+    const index = userIndex.userIndex;
 
     return(
         <div className="people">
-            <img src={users[0].image} className="people--image" />
+            <img src={users[index].image} className="people--image" />
             <div className="people--name">
-                {users[0].name}
+                {users[index].name}
             </div>
             <div className="people--age">
-                {users[0].age}
+                {users[index].age}
             </div>
         </div>
     )
@@ -20,18 +21,25 @@ const People = () =>{
 
 const Home = () =>{
 
+    const[index, setIndex] = React.useState(0);
+
+    React.useEffect(() =>{
+        console.log(index, "番目")
+    })
 
     return (
             <div id="app">
-                <People/>
+                <People
+                    userIndex={index}>
+                </People>
                 <div id="control">
-                    <div className="button no">
+                    <div className="button no"  onClick={()=>setIndex( index + 1)}>
                         <a href="#" className="trigger"></a>
                     </div>
                     <div className="button info">
                         <a href="#" className="trigger"></a>
                     </div>
-                    <div className="button yes">
+                    <div className="button yes" onClick={() =>setIndex(index + 1)}>
                         <a href="#" className="trigger"></a>
                     </div>
                 </div>
