19fc82300ad7f8dbd1fa23d998308a707fa8ed46
wakkihaya <wakkihaya@gmail.com>
2020-05-14 16:46:55 +0900

    copy html,scss from codepen example

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () =>{
+
+
+    return (
+            <div id="app">
+                <div id="people"></div>
+                <div id="control">
+                    <div class="button no">
+                        <a href="#" class="trigger"></a>
+                    </div>
+                    <div class="button info">
+                        <a href="#" class="trigger"></a>
+                    </div>
+                    <div class="button yes">
+                        <a href="#" class="trigger"></a>
+                    </div>
+                </div>
+            </div>
+    );
+}
+
+export default Home;
