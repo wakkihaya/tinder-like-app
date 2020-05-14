62d52d12e523a96922b1338e1930f15fbdd8eb38
wakkihaya <wakkihaya@gmail.com>
2020-05-14 17:12:41 +0900

    Show users' data from json file

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import './Home.scss';
 
+
+const People = () =>{
+    const users = require('./user_sample.json');
+
+    return(
+        <div className="people">
+            <img src={users[0].image} className="people--image" />
+            <div className="people--name">
+                {users[0].name}
+            </div>
+            <div className="people--age">
+                {users[0].age}
+            </div>
+        </div>
+    )
+}
+
 const Home = () =>{
 
 
     return (
             <div id="app">
-                <div id="people"></div>
+                <People/>
                 <div id="control">
-                    <div class="button no">
-                        <a href="#" class="trigger"></a>
+                    <div className="button no">
+                        <a href="#" className="trigger"></a>
                     </div>
-                    <div class="button info">
-                        <a href="#" class="trigger"></a>
+                    <div className="button info">
+                        <a href="#" className="trigger"></a>
                     </div>
-                    <div class="button yes">
-                        <a href="#" class="trigger"></a>
+                    <div className="button yes">
+                        <a href="#" className="trigger"></a>
                     </div>
                 </div>
             </div>
